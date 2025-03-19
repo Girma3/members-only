2f083d0e0606899d7e3f80ddeca9baadfe955a37
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2025-03-19 19:15:41 +0300

    feat: functions created to get all messages,to add new msg  and delete message using id

--- db/queries.js
+++ db/queries.js
@@ -33,27 +33,102 @@ async function getUserById(id) {
 }
 async function updateUserStatusById(id) {
   try {
-    const { rows } = await newPool.query("UPDATE users SET member = true WHERE id = $1", [
-      id,
-    ]);
+    const { rows } = await newPool.query(
+      "UPDATE users SET member = true WHERE id = $1",
+      [id]
+    );
     return rows[0];
   } catch (err) {
     console.error("Error while updating user status:", err);
     throw err;
   }
-  
 }
 async function updateUserToAdminById(id) {
   try {
-    const { rows } = await newPool.query("UPDATE users SET admin = true WHERE id = $1", [
-      id,
-    ]);
+    const { rows } = await newPool.query(
+      "UPDATE users SET admin = true WHERE id = $1",
+      [id]
+    );
     return rows[0];
   } catch (err) {
     console.error("Error while updating user status:", err);
     throw err;
   }
-  
+}
+/*async function getAllMessages() {
+  try {
+    const { rows } = await newPool.query("SELECT * FROM messages");
+    return rows;
+  } catch (err) {
+    console.log(err, "err while getting all messages from db");
+  }
+}
+*/
+async function deleteUser(userId) {
+  try {
+    const { rows } = await newPool.query("DELETE FROM users WHERE id=$1", [
+      userId,
+    ]);
+    return true;
+  } catch (err) {
+    console.log(err, "err while removing user from db");
+  }
+}
+async function deleteMessage(msgId) {
+  try {
+    const { rows } = await newPool.query("DELETE FROM messages WHERE id=$1", [
+      msgId,
+    ]);
+    return true;
+  } catch (err) {
+    console.log(err, "err while removing msg from db");
+  }
 }
 
-export { getUsers, getUserByName, getUserById ,updateUserStatusById,updateUserToAdminById};
+async function addMessages(userId, text) {
+  //format time here
+  let createdTime = new Date().toISOString();
+  try {
+    const { rows } = await newPool.query(
+      "INSERT INTO messages(user_id,text,timestamp) VALUES($1,$2,$3)",
+      [userId, text, createdTime]
+    );
+    return rows;
+  } catch (err) {
+    console.log(err, "err while adding  messages to db");
+  }
+}
+async function getAllMessages() {
+  /*  message form adjusted  in this form:
+  const messages = [
+    { id: 1, author: "king", text: "Good Day", timeStamp: "date" },
+  ];
+  */
+  try {
+    const { rows } = await newPool.query(
+      `SELECT 
+        messages.id, 
+        users.name AS author, 
+        messages.text, 
+        messages.timestamp AS timeStamp 
+      FROM messages 
+      INNER JOIN users ON users.id = messages.user_id`
+    );
+
+    return rows;
+  } catch (err) {
+    console.error("Error while formatting messages:", err);
+    throw err;
+  }
+}
+export {
+  getUsers,
+  getUserByName,
+  getUserById,
+  updateUserStatusById,
+  updateUserToAdminById,
+  deleteUser,
+  getAllMessages,
+  addMessages,
+  deleteMessage,
+};
