8712addacde7c468635f0216f7bd932574cd8665
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2025-03-22 16:42:35 +0300

    fix: response status changed

--- middleware/authenticateMiddleware.js
+++ middleware/authenticateMiddleware.js
@@ -1,21 +1,30 @@
+import bcrypt from "bcryptjs";
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  //guide to log in page
-  return res.status(403).json({ message: "User is not logged in." });
+  return res.status(401).json({ message: "User is not logged in." });
 }
 function isUserAdmin(req, res, next) {
-  if (req.user.admin === true) {
+  if (req.user && req.user.admin === true) {
     return next();
   }
-  return res.status(403).json({ message: "user is not an Admin" });
+  return res.status(401).json({ message: "user is not an Admin" });
 }
 function isUserMember(req, res, next) {
-  if (req.user.member === true) {
+  if (req.user && req.user.member === true) {
     return next();
   }
-  return res.status(403).json({ message: "user is not an member" });
+  return res.status(401).json({ message: "user is not a member" });
+}
+async function hashPassword(password) {
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return hashedPassword;
+  } catch (err) {
+    console.error("Error while hashing password:", err);
+    throw err;
+  }
 }
 
-export { ensureAuthenticated, isUserAdmin, isUserMember };
+export { ensureAuthenticated, isUserAdmin, isUserMember, hashPassword };
